b087c274e0f69291f9731b35df16076a6e6739c0
curtdragon <tealturboser@gmail.com>
2019-05-29 19:43:18 -0500

    header component finished. no CSS

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
 
 function HeaderContainer() {
-    return <img src = "https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"
-    alt = "react Logo"/>
-}
+    return (
+        <div className = "HeaderContainter">
+            <ImageThumbnail/>
+            <HeaderTitle/>
+            <HeaderContent/>
+        </div>
+    );
+};
 
 export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle (){
+    return <h1>Lambda School <span>@LambdaSchool 19:20</span></h1>
+};
+
+export default HeaderTitle;
